513debd1fe2a14c31de8a5224ca45580241e0aac
kewton <newtons.boiled.clock@gmail.com>
2025-04-06 13:43:30 +0900

    最新化

--- aiagent/agent_type2.py
+++ aiagent/agent_type2.py
@@ -1,20 +1,23 @@
 from dotenv import load_dotenv
 from langchain import hub
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import AgentExecutor, create_react_agent
 from tool.google_search_by_gemini import google_search_tool
 from googleapis import send_email_tool, gmail_search_search_tool
+import os
 
 # 環境変数のロード
 load_dotenv()
 
 
-def creatAgentType1():
+def creatAgentType2():
     # HubからReact用プロンプトを取得
     prompt = hub.pull("hwchase17/react")
 
     # LLMの初期化（例: gpt-4o-mini）
-    model = ChatOpenAI(model="gpt-4o-mini")
+    # gemini-1.5-pro
+    # gemini-2.5-pro-exp-03-25
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=os.environ["GEMINI_API_KEY"])
 
     # ツールリストに google_search_tool を含める
     tools = [
@@ -32,6 +35,7 @@ def creatAgentType1():
         tools=tools,
         verbose=True,
         handle_parsing_errors=True,
+        max_iterations=5,
         return_intermediate_steps=True
     )
     return agent_executor
